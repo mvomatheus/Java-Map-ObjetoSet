07690ac7762646fe2bc5728c782a16423279470f
mvomatheus <96183449+mvomatheus@users.noreply.github.com>
2022-01-05 16:33:20 -0300

    Add files via upload

---
+++ colecoes.js
@@ -0,0 +1,18 @@
+function getAdmins(map) {
+    let admins = [];
+    for ([key, value] of map) {
+        if (value === 'Admin') {
+            admins.push(key);
+        }
+    }
+    return admins;
+}
+
+const usuarios = new Map();
+
+usuarios.set('Luiz', 'Admin');
+usuarios.set('Stephany', 'Admin');
+usuarios.set('Matheus', 'User');
+usuarios.set('Bruno', 'Admin');
+
+console.log(getAdmins(usuarios));
